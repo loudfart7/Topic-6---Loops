63d0b7d15f442e0b4097c6f21fb7be54d32f2dd8
Chloe Vandeworp <339639510@GoTVDSB.ca>
2025-03-19 14:40:05 -0400

    menu + static methods

--- Program.cs
+++ Program.cs
@@ -11,8 +11,9 @@
                 Console.Clear();
                 Console.WriteLine("Welcome to my generic menu.  Please select an option:");
                 Console.WriteLine();
-                Console.WriteLine("1 - Menu Option 1");
-                Console.WriteLine("2 - Menu Option 2");
+                Console.WriteLine("1 - Prompter");
+                Console.WriteLine("2 - BOB Banking");
+                Console.WriteLine("3 - Doubles Roller") ;
                 Console.WriteLine("...");
                 Console.WriteLine("x - Menu Option x");
                 Console.WriteLine("Q - Quit");
@@ -47,7 +48,7 @@
 
                 else
                 {
-                    Console.WriteLine("Invalid choice, press ENTER to continue.");
+                    Console.WriteLine("Invalid choice, please press ENTER to continue.");
                     Console.ReadLine();
                 }
 
@@ -56,20 +57,130 @@
 
         static void prompter()
         {
+            Console.Clear();
+            int min, max, input;
+            bool done = false;
+
             Console.WriteLine();
             Console.WriteLine("PROMPTER");
             Console.WriteLine();
+
+            Console.Write("Enter a min value and amax for your randome number generator: ");
             Console.WriteLine();
+
+            Console.Write("MIN: ");
+            while (!int.TryParse(Console.ReadLine(), out min))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Invalid input, please try again. Input must only contain numbers can must not contain letters or special characters.");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            Console.Write("MAX: ");
+            while (!int.TryParse(Console.ReadLine(), out max))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Invalid input, please try again. Input must only contain numbers can must not contain letters or special characters.");
+                Console.WriteLine();
+            }
+
+            if (min > max)
+            {
+                int temp = min;
+                min = max;
+                max = temp;
+            }
+
+            Console.WriteLine();
+
+            //loop
+            while (!done)
+            {
+                Console.WriteLine("Enter in a value between " + min + " and " + max + ":");
+
+                while (!int.TryParse(Console.ReadLine(), out input))
+                    Console.WriteLine("Invalid input. Please try again.");
+
+                if (input >= min && input <= max)
+                    done = true;
+                else
+                    Console.WriteLine("Please enter valid input between " + min + " and " + max + ".");
+            }
+
         }
 
         static void banking()
         {
-            Console.WriteLine();
-        }
+            Console.Clear();
+            double balance = 150;
 
-        static void doublesRoller()
-        {
             Console.WriteLine();
+            Console.WriteLine("PLANET BLORB BANK");
+            Console.WriteLine();
+
+            string bankSelection = "";
+
+            while (bankSelection != "q")
+            {
+                Console.Clear();
+                Console.WriteLine("BANK OF BLORB MENU. PLEASE SELECT ONOF THE FOLLOWING ACTIONS:");
+                Console.WriteLine();
+                Console.WriteLine("Deposit");
+                Console.WriteLine("Withdrawl");
+                Console.WriteLine("Bill Payment");
+                Console.WriteLine();
+                Console.WriteLine("x - Menu Option x");
+                Console.WriteLine("Q - Quit");
+                Console.WriteLine();
+                bankSelection = Console.ReadLine().ToLower().Trim();
+                Console.WriteLine();
+
+                if (bankSelection == "deposit")
+                {
+                    Console.WriteLine("You chose option 1");
+                    Console.WriteLine("Hit ENTER to continue.");
+                    Console.ReadLine();
+
+                    prompter();
+                }
+                else if (bankSelection == "withdrawl")
+                {
+                    Console.WriteLine("You chose option 2");
+                    Console.WriteLine("Hit ENTER to continue.");
+                    Console.ReadLine();
+
+                    banking();
+                }
+                else if (bankSelection == "Bill Payment")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.ReadLine();
+
+                    doublesRoller();
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid choice, please press ENTER to continue.");
+                    Console.ReadLine();
+                }
+
+
+            }
+
+            static void doublesRoller()
+            {
+                Console.Clear();
+
+                Console.WriteLine();
+                Console.WriteLine("PLANET BLORB BANK");
+                Console.WriteLine();
+
+            }
         }
     }
+
 }
